727533652a030f95f95841be6ef1545a071fcd36
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2019-02-09 16:30:33 +0000

    Make downloading from YouTube asynchronous

--- youtube.py
+++ youtube.py
@@ -1,16 +1,38 @@
+import logging
 import pathlib
 
+import aiofiles
+import aiohttp
 import pafy
 
 from library import MusicInfo
 
+logger = logging.getLogger(__name__)
 
-def download(url: str, download_dir: pathlib.Path):
+CHUNK_SIZE = 131072  # 128KiB
+
+
+async def download(session: aiohttp.ClientSession, url: str,
+                   download_dir: pathlib.Path):
     video = pafy.new(url)
     stream = video.getbestaudio()
     file_name = video.videoid + '.' + stream.extension
     path = download_dir.joinpath(file_name)
 
-    stream.download(path)
+    logger.info('Downloading video {} with URL: {}', video.videoid, stream.url)
+    async with session.get(stream.url) as resp:
+        if resp.status == 200:
+            logger.info('Opening file {}', path)
+            async with aiofiles.open(path, 'wb') as file:
+                while True:
+                    chunk = await resp.content.read(CHUNK_SIZE)
+                    if not chunk:
+                        break
+                    await file.write(chunk)
+        else:
+            logger.error('Server responded with status {}: {}', resp.status,
+                         resp.reason)
+            return None
 
+    logger.info('Video {} downloaded successfully', video.videoid)
     return MusicInfo(video.videoid, video.title, video.length, file_name)

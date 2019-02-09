2e5645db5b828cdf5296f251d33f3a02b70c417a
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2019-02-09 17:09:40 +0000

    Add error handling to the YouTube download function

--- youtube.py
+++ youtube.py
@@ -14,25 +14,39 @@ CHUNK_SIZE = 131072  # 128KiB
 
 async def download(session: aiohttp.ClientSession, url: str,
                    download_dir: pathlib.Path):
-    video = pafy.new(url)
-    stream = video.getbestaudio()
+    try:
+        video = pafy.new(url)
+    except Exception as e:
+        logger.error('Failed to get video info: {}', e)
+        return
+
+    stream = video.getbestaudio(ftypestrict=False)
+    if stream is None:
+        logger.error('Video {} does not have any audio streams', video.videoid)
+        return
+
     file_name = video.videoid + '.' + stream.extension
     path = download_dir.joinpath(file_name)
 
     logger.info('Downloading video {} with URL: {}', video.videoid, stream.url)
-    async with session.get(stream.url) as resp:
-        if resp.status == 200:
-            logger.info('Opening file {}', path)
-            async with aiofiles.open(path, 'wb') as file:
-                while True:
-                    chunk = await resp.content.read(CHUNK_SIZE)
-                    if not chunk:
-                        break
-                    await file.write(chunk)
-        else:
-            logger.error('Server responded with status {}: {}', resp.status,
-                         resp.reason)
-            return None
+    try:
+        async with session.get(stream.url) as resp:
+            if resp.status == 200:
+                logger.info('Opening file {}', path)
+                async with aiofiles.open(path, 'wb') as file:
+                    while True:
+                        chunk = await resp.content.read(CHUNK_SIZE)
+                        if not chunk:
+                            break
+                        await file.write(chunk)
+            else:
+                logger.error('Server responded with status {}: {}',
+                             resp.status, resp.reason)
+                return
+    except Exception as e:
+        logger.error('Error downloading {} to file {}: {}',
+                     stream.url, path, e)
+        return
 
     logger.info('Video {} downloaded successfully', video.videoid)
     return MusicInfo(video.videoid, video.title, video.length, file_name)

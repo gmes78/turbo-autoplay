d759188e1370014cfa5b424a7d5ac3071585315f
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2019-02-09 16:00:02 +0000

    Fix getting video id from Pafy

--- youtube.py
+++ youtube.py
@@ -8,9 +8,9 @@ from library import MusicInfo
 def download(url: str, download_dir: pathlib.Path):
     video = pafy.new(url)
     stream = video.getbestaudio()
-    file_name = video.id + '.' + stream.extension
+    file_name = video.videoid + '.' + stream.extension
     path = download_dir.joinpath(file_name)
 
     stream.download(path)
 
-    return MusicInfo(video.id, video.title, video.length, file_name)
+    return MusicInfo(video.videoid, video.title, video.length, file_name)

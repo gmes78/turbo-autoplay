8a26333755cfa10263813be65f2ebad99dbe77bb
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2019-02-09 13:48:46 +0000

    Add serialization and deserialization functionality to the music information data structures

--- library.py
+++ library.py
@@ -1,17 +1,24 @@
+import json
 import random
 
 
 class MusicInfo:
-    def __init__(self, video_id, title, length, file_name):
+    def __init__(self, video_id: str, title: str, length: int, file_name: str):
         self.id = video_id
         self.title = title
         self.length = length
         self.file_name = file_name
 
 
+class MusicInfoEncoder(json.JSONEncoder):
+    def default(self, o: MusicInfo):
+        return {'file_name': o.file_name, 'id': o.id, 'length': o.length,
+                'title': o.title}
+
+
 class MusicLibrary:
-    def __init__(self):
-        self.musics = []
+    def __init__(self, musics: [MusicInfo] = []):
+        self.musics = musics
 
     def add(self, music: MusicInfo):
         self.musics.append(music)
@@ -27,3 +34,17 @@ class MusicLibrary:
     def get_random_with_max_len(self, max_length: int):
         eligible_musics = [m for m in self.musics if m.length <= max_length]
         return random.choice(eligible_musics)
+
+    def into_json(self, pretty=True):
+        if pretty:
+            return MusicInfoEncoder(indent=4).encode(self.musics)
+        else:
+            return MusicInfoEncoder().encode(self.musics)
+
+    @staticmethod
+    def from_json(json_data):
+        data = json.loads(json_data)
+        musics = [
+            MusicInfo(o['id'], o['title'], o['length'], o['file_name'])
+            for o in data]
+        return MusicLibrary(musics)

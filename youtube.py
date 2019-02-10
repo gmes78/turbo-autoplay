52b8173326c0d25213f72cca715f1724e9430314
Joaquim Monteiro <joaquim.monteiro@protonmail.com>
2019-02-10 21:49:47 +0000

    Fix log formatting in the youtube module

--- youtube.py
+++ youtube.py
@@ -17,22 +17,22 @@ async def download(session: aiohttp.ClientSession, url: str,
     try:
         video = pafy.new(url)
     except Exception as e:
-        logger.error('Failed to get video info: {}', e)
+        logger.error(f'Failed to get video info: {e}')
         return
 
     stream = video.getbestaudio(ftypestrict=False)
     if stream is None:
-        logger.error('Video {} does not have any audio streams', video.videoid)
+        logger.error(f'Video {video.videoid} does not have any audio streams')
         return
 
     file_name = video.videoid + '.' + stream.extension
     path = download_dir.joinpath(file_name)
 
-    logger.info('Downloading video {} with URL: {}', video.videoid, stream.url)
+    logger.info(f'Downloading video {video.videoid} with URL: {stream.url}')
     try:
         async with session.get(stream.url) as resp:
             if resp.status == 200:
-                logger.info('Opening file {}', path)
+                logger.info(f'Opening file {path}')
                 async with aiofiles.open(path, 'wb') as file:
                     while True:
                         chunk = await resp.content.read(CHUNK_SIZE)
@@ -40,13 +40,12 @@ async def download(session: aiohttp.ClientSession, url: str,
                             break
                         await file.write(chunk)
             else:
-                logger.error('Server responded with status {}: {}',
-                             resp.status, resp.reason)
+                logger.error(f'Server responded with status {resp.status}: '
+                             '{resp.reason}')
                 return
     except Exception as e:
-        logger.error('Error downloading {} to file {}: {}',
-                     stream.url, path, e)
+        logger.error(f'Error downloading {stream.url} to file {path}: {e}')
         return
 
-    logger.info('Video {} downloaded successfully', video.videoid)
+    logger.info(f'Video {video.videoid} downloaded successfully')
     return MusicInfo(video.videoid, video.title, video.length, file_name)
